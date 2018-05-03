446948808182444ee1537441fb5b00a394557382
kamil <2769389733@qq.com>
2018-05-03 09:52:37 +0800

    add WelcomeActivity

---
+++ app/src/main/java/com/example/administrator/comicforum/WelcomeActivity.java
@@ -0,0 +1,38 @@
+package com.example.administrator.comicforum;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
+import android.os.PersistableBundle;
+import android.support.annotation.Nullable;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.ImageView;
+
+/**
+ * Created by Administrator on 2018/5/3.
+ */
+
+public class WelcomeActivity extends Activity {
+    private ImageView imageWelcome;
+    public void onCreate(@Nullable Bundle savedInstanceState, @Nullable PersistableBundle persistentState) {
+        super.onCreate(savedInstanceState, persistentState);
+        setContentView(R.layout.activity_welcome);
+        handler.sendEmptyMessageDelayed(0,3000);
+    }
+    private Handler handler = new Handler() {
+        @Override
+        public void handleMessage(Message msg) {
+            getHome();
+            super.handleMessage(msg);
+        }
+    };
+
+    public void getHome(){
+        Intent intent = new Intent(WelcomeActivity.this, MainActivity.class);
+        startActivity(intent);
+        finish();
+    }
+}
