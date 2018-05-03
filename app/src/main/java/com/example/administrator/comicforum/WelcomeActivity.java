fea32a3e348355c6a44eaa65c9e2a82575e6315f
kamil <2769389733@qq.com>
2018-05-03 14:53:04 +0800

    add WelcomeActivity

--- app/src/main/java/com/example/administrator/comicforum/WelcomeActivity.java
+++ app/src/main/java/com/example/administrator/comicforum/WelcomeActivity.java
@@ -5,34 +5,32 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Message;
-import android.os.PersistableBundle;
 import android.support.annotation.Nullable;
-import android.view.Window;
-import android.view.WindowManager;
-import android.widget.ImageView;
+
+import java.util.Timer;
+import java.util.TimerTask;
 
 /**
  * Created by Administrator on 2018/5/3.
  */
 
 public class WelcomeActivity extends Activity {
-    @Override
+    private Intent intent;
+    private Timer timer;
+    private TimerTask task;
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome);
-        handler.sendEmptyMessageDelayed(0,3000);
+        intent=new Intent(this,MainActivity.class);
+        timer=new Timer();
+        task=new TimerTask() {
+            @Override
+            public void run() {
+                startActivity(intent);
+                finish();
+            }
+        };
+        timer.schedule(task,3000);
     }
-    private Handler handler = new Handler() {
-        @Override
-        public void handleMessage(Message msg) {
-            getHome();
-            super.handleMessage(msg);
-        }
-    };
 
-    public void getHome(){
-        Intent intent = new Intent(WelcomeActivity.this, MainActivity.class);
-        startActivity(intent);
-        finish();
-    }
 }
